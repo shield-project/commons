6216776dce8c7a173b62fdf23788ae98e27175ed
ShawnShoper <xiehao,930825>
2018-08-22 16:35:16 +0800

    add encryption

---
+++ commons-encryption/src/main/java/com/ruiqi/commons/encryption/config/EncryptUtil.java
@@ -0,0 +1,58 @@
+package com.ruiqi.commons.encryption.config;
+
+import lombok.extern.log4j.Log4j;
+import org.jasypt.util.password.StrongPasswordEncryptor;
+import org.jasypt.util.text.StrongTextEncryptor;
+
+/**
+ * @author ShawnShoper
+ * @date 2018/7/18 11:22
+ */
+@Log4j
+public class EncryptUtil {
+
+    public static final String prefix = "\u0020";
+    public static final String text_prefix = prefix + "t";
+    public static final String password_prefix = prefix + "p";
+
+    /**
+     * Passworld encryptor.
+     * 提供密码校验器
+     */
+    public static class PassworldEncryptor {
+        StrongPasswordEncryptor encryptor = new StrongPasswordEncryptor();
+
+        public String encrypt(String text) {
+            return password_prefix + this.encryptor.encryptPassword(text);
+        }
+
+        public boolean check(String password, String encrypted) {
+            if (!encrypted.startsWith(password_prefix))
+                throw new IllegalArgumentException("校验值类型不正确,缺少Password校验位");
+            return this.encryptor.checkPassword(password, encrypted.replace(password_prefix, ""));
+        }
+    }
+
+    /**
+     * Text encryptor
+     * 提供文本加密机
+     */
+    public static class TextEncryptor {
+        StrongTextEncryptor strongTextEncryptor = new StrongTextEncryptor();
+
+        public TextEncryptor(String secretKey) {
+            this.strongTextEncryptor.setPassword(secretKey);
+        }
+
+        public String encrypt(String text) {
+            return text_prefix + this.strongTextEncryptor.encrypt(text);
+        }
+
+        public String decrypt(String text) {
+            if (!text.startsWith(text_prefix))
+                throw new IllegalArgumentException("校验值类型不正确,缺少Text校验位");
+            return this.strongTextEncryptor.decrypt(text.replace(text_prefix, ""));
+        }
+    }
+
+}
---
+++ commons-encryption/src/main/java/com/ruiqi/commons/encryption/config/EncryptionPropertyResolver.java
@@ -0,0 +1,39 @@
+package com.ruiqi.commons.encryption.config;
+
+import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyResolver;
+import lombok.extern.log4j.Log4j;
+import org.jasypt.exceptions.EncryptionOperationNotPossibleException;
+import org.springframework.util.StringUtils;
+
+@Log4j
+public class EncryptionPropertyResolver implements EncryptablePropertyResolver {
+    private EncryptUtil.TextEncryptor textEncryptor;
+
+    public EncryptionPropertyResolver() {
+    }
+
+    public EncryptionPropertyResolver(EncryptUtil.TextEncryptor textEncryptor) {
+        this.textEncryptor = textEncryptor;
+    }
+
+    @Override
+    public String resolvePropertyValue(String value) {
+        if (StringUtils.isEmpty(value)) {
+            return value;
+        }
+        if (value.startsWith(EncryptUtil.text_prefix))
+            return resolveDESValue(value);
+        // 不需要解密的值直接返回
+        return value;
+    }
+
+    private String resolveDESValue(String value) {
+        String result = value;
+        try {
+            result = textEncryptor.decrypt(value);
+        } catch (EncryptionOperationNotPossibleException e) {
+            log.warn("Found need decryption property,but the secret-key maybe not matched,return the original value " + value);
+        }
+        return result;
+    }
+}
